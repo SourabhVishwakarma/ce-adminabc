185a0fa6d59356811aa67a49e3c056cb331844a4
ce-dev-ops <devops@concordeeye.com>
2021-06-13 17:57:47 +0530

    First Commit

---
+++ src/main/java/concordeeye/io/ConcordeEyeAdmin/ConcordeEyeAdminApplication.java
@@ -0,0 +1,13 @@
+package concordeeye.io.ConcordeEyeAdmin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcordeEyeAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcordeEyeAdminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/concordeeye/io/ConcordeEyeAdmin/ConcordeEyeAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package concordeeye.io.ConcordeEyeAdmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcordeEyeAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
